aa1e9fbfbf21daea5382ca95aa9bfdc4f3574bde
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-08-13 19:48:45 +0530

    Added one more tool and checking

--- 5_MCP/mcp.py
+++ 5_MCP/mcp.py
@@ -1,4 +1,7 @@
 from fastmcp import FastMCP
+from langchain_community.tools import DuckDuckGoSearchResults
+
+search_tool = DuckDuckGoSearchResults()
 
 mcp = FastMCP("Demo")
 
@@ -6,6 +9,12 @@ mcp = FastMCP("Demo")
 def add(a:int, b:int) -> int:
     """ Adds two numbers """
     return a+b
+
+@mcp.tool
+def search(query: str) -> str:
+    """ Searches about the query and provides the answer """
+    result = search_tool.invoke(query)
+    return result
 
 if __name__ == "__main__":
     mcp.run()
